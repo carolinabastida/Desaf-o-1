a97f8d70ebe5d262551c28b9ee5002d39dd132ad
carolinabastida <cbastida11@gmail.com>
2025-05-03 11:34:02 -0300

    Add files via upload

---
+++ Desafío Suma.py
@@ -0,0 +1,6 @@
+print("Esto es una suma")
+numeroUno = 9
+numeroDos = 2
+resultado = numeroUno+numeroDos
+print(resultado)
+
---
+++ Desafío rectángulo.py
@@ -0,0 +1,11 @@
+print("Calcular el área de un rectángulo")
+ancho = 2
+largo = 9
+area = largo * ancho
+resultado = largo * ancho
+print("Este programa calcula el área de un rectángulo dado su ancho y su largo")
+print(resultado)
+
+
+
+    
